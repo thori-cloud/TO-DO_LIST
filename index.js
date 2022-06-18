33e98c2c40145c84d29b8af669951cbf36d1c07d
thori-cloud <thoria473@gmail.com>
2022-06-18 11:41:32 +0530

    final-one

--- index.js
+++ index.js
@@ -14,89 +14,88 @@ app.use(urlEncodedParser);
 app.use(express.static("public"));
 
 const taskSchema = new mongoose.Schema({
-  title: String,
-  description: String,
+    title: String,
+    description: String,
 });
 
 const Task = mongoose.model("Task", taskSchema);
 
-app.get("/", function (req, res) {
-  Task.find({}, function (err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("todo", { tasks: data });
-    }
-  });
+app.get("/", function(req, res) {
+    Task.find({}, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("todo", { tasks: data });
+        }
+    });
 });
 
-app.post("/", function (req, res) {
-  const task = new Task({
-    title: req.body.title,
-    description: req.body.description,
-  });
-  task.save((err, task) => {
-    if (err) {
-      res.redirect("error");
-    } else {
-      res.redirect("/");
-    }
-  });
+app.post("/", function(req, res) {
+    const task = new Task({
+        title: req.body.title,
+        description: req.body.description,
+    });
+    task.save((err, task) => {
+        if (err) {
+            res.redirect("error");
+        } else {
+            res.redirect("/");
+        }
+    });
 });
 
-app.get("/edit/:id", function (req, res) {
-  var id = req.params.id;
+app.get("/edit/:id", function(req, res) {
+    var id = req.params.id;
 
-  Task.find({}, function (err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("edit", { tasks: data, id: id });
-    }
-  });
+    Task.find({}, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("edit", { tasks: data, id: id });
+        }
+    });
 });
 
-app.post("/edit/:id", function (req, res) {
-  var id = req.params.id;
+app.post("/edit/:id", function(req, res) {
+    var id = req.params.id;
 
-  Task.findByIdAndUpdate(
-    id,
-    {
-      title: req.body.title,
-      description: req.body.description,
-    },
-    function (err, data) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.redirect("/");
-      }
-    }
-  );
+    Task.findByIdAndUpdate(
+        id, {
+            title: req.body.title,
+            description: req.body.description,
+        },
+        function(err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect("/");
+            }
+        }
+    );
 });
 
-app.get("/delete/:id", function (req, res) {
-  Task.deleteOne({ _id: req.params.id }, function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect("/");
-    }
-  });
+app.get("/delete/:id", function(req, res) {
+    Task.deleteOne({ _id: req.params.id }, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
 });
 
-app.get("/edit2/:id", function (req, res) {
-  var id = req.params.id;
+app.get("/edit2/:id", function(req, res) {
+    var id = req.params.id;
 
-  Task.findById(id, function (err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("edit2", { task: data });
-    }
-  });
+    Task.findById(id, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("edit2", { task: data });
+        }
+    });
 });
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server is running on port 3000");
 });
